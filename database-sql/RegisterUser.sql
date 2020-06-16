929c6dda1e73f3b50ef508109d623bad2aab1a5b
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-06-16 08:04:22 -0400

    initialize the database project create registeruser procedure create the backend user update some columns

---
+++ database-sql/RegisterUser.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[RegisterUser]
+	@FirstName nvarchar(80),
+	@LastName nvarchar(80),
+	@RegistrationOrg int,
+	@Email nvarchar(200),
+	@phoneNumber char(14),
+	@Address nvarchar(200),
+	@FamilyID UniqueIdentifier,
+	@IsMale bit
+AS
+IF @RegistrationOrg is null 
+Set @RegistrationOrg=0
+
+Insert into dbo.visitor
+(RegistrationOrg,FirstName,LastName,Email,PhoneNumber,[Address],FamilyID,IsMale)
+Values (@RegistrationOrg,@FirstName,@LastName,@Email,@phoneNumber,@Address,@FamilyID,@IsMale)
---
+++ database-sql/dbo/Tables/visitor.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[visitor] (
+    [Id]              UNIQUEIDENTIFIER CONSTRAINT [DF_visitor_Id] DEFAULT (newid()) NOT NULL,
+    [RegistrationOrg] INT              NULL,
+    [FirstName]       NVARCHAR (80)    NOT NULL,
+    [LastName]        NVARCHAR (80)    NOT NULL,
+    [Email]           NVARCHAR (200)   NOT NULL,
+    [PhoneNumber]     CHAR (14)        NOT NULL,
+    [Address]         NVARCHAR (200)   NULL,
+    [FamilyID]        UNIQUEIDENTIFIER NULL,
+    [IsMale]          BIT              NOT NULL,
+    CONSTRAINT [PK_visitors] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_visitors_organization] FOREIGN KEY ([RegistrationOrg]) REFERENCES [dbo].[organization] ([Id])
+);
+
