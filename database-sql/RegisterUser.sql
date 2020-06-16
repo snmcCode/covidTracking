a848bcdd46bc835a35056f10cfeb3c5bccab5c26
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-06-16 19:52:27 -0400

    registerUser to isnert in visitor table #5

--- database-sql/RegisterUser.sql
+++ database-sql/RegisterUser.sql
@@ -6,11 +6,26 @@
 	@phoneNumber char(14),
 	@Address nvarchar(200),
 	@FamilyID UniqueIdentifier,
-	@IsMale bit
+	@IsMale bit,
+	@recordID UniqueIdentifier output
 AS
 IF @RegistrationOrg is null 
 Set @RegistrationOrg=0
 
-Insert into dbo.visitor
-(RegistrationOrg,FirstName,LastName,Email,PhoneNumber,[Address],FamilyID,IsMale)
-Values (@RegistrationOrg,@FirstName,@LastName,@Email,@phoneNumber,@Address,@FamilyID,@IsMale)
+Declare @insertedValue Table(Id UniqueIdentifier)
+Begin Try
+	Insert dbo.visitor(RegistrationOrg,FirstName,LastName,Email,PhoneNumber,[Address],FamilyID,IsMale)
+	OUTPUT inserted.Id into @insertedValue 
+	Values (@RegistrationOrg,@FirstName,@LastName,@Email,@phoneNumber,@Address,@FamilyID,@IsMale)
+End Try
+Begin Catch
+	IF ERROR_NUMBER() = 2601 or ERROR_NUMBER()=2627 
+	Begin 
+		Insert into @insertedValue Select top 1 id from dbo.visitor 
+		where FirstName=@FirstName and LastName=@LastName and PhoneNumber=@phoneNumber
+	End
+	Else
+		Throw;
+End Catch 
+
+Select @recordID=id from @insertedValue
--- database-sql/dbo/Tables/organization.sql
+++ database-sql/dbo/Tables/organization.sql
@@ -7,4 +7,9 @@
     [ContanctEmail] NVARCHAR(100) NULL, 
     CONSTRAINT [PK_organization] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+GO
+SET IDENTITY_INSERT dbo.organization ON;
 
+INSERT INTO dbo.organization(id,Name) Values(0,'online')
+
+SET IDENTITY_INSERT dbo.organization OFF;
