08ee22b51c7e27230d55df02633ab430db3c5551
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-06-22 16:29:14 -0400

    new project and gitignore updates

---
+++ database-sql/StoredProcedures/GetUser.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[GetUser]
+	@userID UniqueIdentifier=null,
+	@FirstName nvarchar(80)=null,
+	@LastName nvarchar(80)=null,
+	@Email nvarchar(200)=null,
+	@phoneNumber char(14)=null
+AS
+	IF @userID IS not null 
+	BEGIN 
+		Select * from dbo.visitor where id=@userID
+	END
+	ELSE 
+	BEGIN 
+		Select * from dbo.visitor 
+		where (@FirstName is null or FirstName=@FirstName) and 
+		(@LastName is null or LastName=@LastName) and 
+		(@phoneNumber is null or PhoneNumber=@phoneNumber ) and 
+		(@Email is null or Email=@Email)
+	END
+	
