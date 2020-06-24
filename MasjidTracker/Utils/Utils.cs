0f04924e34d58d7493e3b34ed529cfb78cb9a93c
Adi Khader <akhader3035@gmail.com>
2020-06-23 18:43:18 -0700

    app pointing to new url

--- MasjidTracker/Utils/Utils.cs
+++ MasjidTracker/Utils/Utils.cs
@@ -10,9 +10,9 @@ namespace FrontEnd
 {
     public class Utils
     {
-        public static string RETRIEVE_USER_API_URL = "https://snmtrackingapi.azurewebsites.net/api/user/{0}?code=K80O15HVIhCZ4V9jpX9YbAuLZT0vLU0W1EIdL30sZx5/S/Vrts9SkQ==";
-        public static string RETRIEVE_USERS_API_URL = "https://snmtrackingapi.azurewebsites.net/api/users?code=r7BbPBZlZwufoowujMT9bypwUzPJ/KaNdc0qRd2uGpoOQFLhCIe1qQ==&FirstName={0}&LastName={1}&PhoneNumber={2}";
-        public static string REGISTER_API_URL = "https://snmtrackingapi.azurewebsites.net/api/user?code=gwQgJAGWvdkvykPEUcFhhkgJ2naRFAUqK029Rd/MruwA9APMgiOPug==";
+        public static string RETRIEVE_USER_API_URL = "https://api.track.mysnmc.ca/api/user/{0}?code=K80O15HVIhCZ4V9jpX9YbAuLZT0vLU0W1EIdL30sZx5/S/Vrts9SkQ==";
+        public static string RETRIEVE_USERS_API_URL = "https://api.track.mysnmc.ca/api/users?code=r7BbPBZlZwufoowujMT9bypwUzPJ/KaNdc0qRd2uGpoOQFLhCIe1qQ==&FirstName={0}&LastName={1}&PhoneNumber={2}";
+        public static string REGISTER_API_URL = "https://api.track.mysnmc.ca/api/user?code=gwQgJAGWvdkvykPEUcFhhkgJ2naRFAUqK029Rd/MruwA9APMgiOPug==";
         public static Byte[] BitmapToBytesCode(Bitmap image)
         {
             using (MemoryStream stream = new MemoryStream())
