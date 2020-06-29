796e5fdc5387d33e0428efa22b0e51ef2f7260dc
ghufrannoor2 <ghufrannoor@Ghufrans-MacBook-Pro.local>
2020-06-29 16:44:41 -0600

    Ghufran - Scanner work, first commit

---
+++ Scanner/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Scanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            SettingsPage SettingsPage = new SettingsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
