1e2866ca43ffd841c1fbaf1fb78635df0ea578b8
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-07-02 08:07:46 -0400

    add organizationDoor table

---
+++ database-sql/dbo/Tables/organizationDoor.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[organizationDoor](
+	[orgID] [int] NOT NULL,
+	[Door] [varchar](50) NOT NULL,
+	CONSTRAINT [FK_organization_organizatoinDoor] FOREIGN KEY ([orgID]) REFERENCES [dbo].[organization] ([Id])
+) 
+
+ON [PRIMARY]
