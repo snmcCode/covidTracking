79400fe4ba4daf290935d853e02f1eb1f353451d
ghufrannoor2 <ghufrannoor@gmail.com>
2020-07-02 07:05:49 -0600

    Added LoginPage and SettingsPage, added custom renderers for Entry and Picker components for iOS and Android, small crash during navigation from SettingsPage back to LoginPage on Android, but that's okay for now as the current implementation of navigation is temporary and is only used to inspect the screens

---
+++ Scanner.Android/Renderers/CustomPickerRenderer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Scanner.Visuals;
+using Scanner.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.Android;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer), new[] { typeof(CustomVisual) })]
+namespace Scanner.Droid.Renderers
+{
+    public class CustomPickerRenderer : MaterialPickerRenderer
+    {
+        GradientDrawable UnfocusedBackground = new GradientDrawable();
+        GradientDrawable FocusedBackground = new GradientDrawable();
+
+        public CustomPickerRenderer(Context context) : base(context)
+        {
+            GenerateBackgrounds();
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control != null)
+                {
+                    Control.EditText.Background = null;
+                    Control.EditText.SetBackgroundColor(Android.Graphics.Color.Transparent);
+                    Control.SetBackground(UnfocusedBackground);
+                }
+
+                e.NewElement.Unfocused += (sender, evt) =>
+                {
+                    Control.SetBackground(UnfocusedBackground);
+                };
+                e.NewElement.Focused += (sender, evt) =>
+                {
+                    Control.SetBackground(FocusedBackground);
+                };
+            }
+
+            if (e.OldElement != null)
+            {
+
+            }
+
+        }
+
+        private void GenerateBackgrounds()
+        {
+            UnfocusedBackground.SetShape(ShapeType.Rectangle);
+            UnfocusedBackground.SetColor(Resources.GetColor(Resource.Color.launcher_background, null));
+            UnfocusedBackground.SetCornerRadii(new float[] { 10, 10, 10, 10, 10, 10, 10, 10 });
+            UnfocusedBackground.SetStroke(2, Resources.GetColor(Resource.Color.colorTextSecondary, null));
+
+            FocusedBackground.SetShape(ShapeType.Rectangle);
+            FocusedBackground.SetColor(Resources.GetColor(Resource.Color.launcher_background, null));
+            FocusedBackground.SetCornerRadii(new float[] { 10, 10, 10, 10, 10, 10, 10, 10 });
+            FocusedBackground.SetStroke(4, Resources.GetColor(Resource.Color.colorAccent, null));
+        }
+    }
+}
--- Scanner/SettingsPage.xaml.cs
+++ Scanner/SettingsPage.xaml.cs
@@ -8,12 +8,9 @@ using Xamarin.Forms;
 
 namespace Scanner
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class SettingsPage : ContentPage
     {
-        public MainPage()
+        public SettingsPage()
         {
             InitializeComponent();
         }
