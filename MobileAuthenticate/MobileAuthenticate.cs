0e9ab79464e3055ec638f3d02a45554a3031494f
ghufrannoor2 <ghufrannoor@gmail.com>
2020-07-08 19:43:36 -0600

    Issue 89 And More:
    Modified Android Main Layout, added new project for authentication and added authentication API, waiting for stored procedure

---
+++ MobileAuthenticate/MobileAuthenticate.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+using Common.Models;
+using Common.Resources;
+using MobileAuthenticate.Utilities;
+using MobileAuthenticate.Utilities.Exceptions;
+
+namespace MobileAuthenticate
+{
+    public static class MobileAuthenticate
+    {
+        [FunctionName("MobileAuthenticate")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "authenticate")] HttpRequest req,
+            ILogger log, ExecutionContext context)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            log.LogInformation("UpdateVisitor Invoked");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            log.LogInformation(requestBody);
+            Organization organization = null;
+            bool success = true;
+            int StatusCode = CustomStatusCodes.PLACEHOLDER;
+            string ErrorMessage = CustomStatusCodes.GetStatusCodeDescription(StatusCode);
+
+            try
+            {
+                ScannerLogin scannerLogin = JsonConvert.DeserializeObject<ScannerLogin>(requestBody);
+                DatabaseManager databaseManager = new DatabaseManager(scannerLogin, log, config);
+                organization = databaseManager.LoginScanner();
+            }
+
+            catch (BadRequestBodyException e)
+            {
+                log.LogError(e.Message);
+                success = false;
+                StatusCode = CustomStatusCodes.BADBUTVALIDREQUESTBODY;
+                ErrorMessage = CustomStatusCodes.GetStatusCodeDescription(StatusCode);
+            }
+
+            catch (SqlDatabaseException e)
+            {
+                log.LogError(e.Message);
+                success = false;
+                StatusCode = CustomStatusCodes.SQLDATABASEERROR;
+                ErrorMessage = CustomStatusCodes.GetStatusCodeDescription(StatusCode);
+            }
+
+            catch (SqlDatabaseDataNotFoundException e)
+            {
+                log.LogError(e.Message);
+                success = false;
+                StatusCode = CustomStatusCodes.NOTFOUNDINSQLDATABASE;
+                ErrorMessage = $"Organization: {CustomStatusCodes.GetStatusCodeDescription(StatusCode)}";
+            }
+
+            return success
+                ? (ActionResult)new OkObjectResult(organization)
+                : new ObjectResult(ErrorMessage)
+                { StatusCode = StatusCode };
+        }
+    }
+}
---
+++ MobileAuthenticate/Utilities/Exceptions/SqlDatabaseException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MobileAuthenticate.Utilities.Exceptions
+{
+    [Serializable]
+    public class SqlDatabaseException : Exception
+    {
+        public SqlDatabaseException() { }
+
+        public SqlDatabaseException(string message) : base(message) { }
+
+        public SqlDatabaseException(string message, Exception inner): base(message, inner) { }
+    }
+}
