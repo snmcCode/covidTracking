8d57cd1fe96e8235cad85e08f45bcd49e7dca3ec
ghufrannoor2 <ghufrannoor@gmail.com>
2020-07-28 19:20:37 -0600

    Fixed typos

--- Scanner/Android/app/src/main/java/ca/snmc/scanner/utils/ErrorCodes.kt
+++ Scanner/Android/app/src/main/java/ca/snmc/scanner/utils/ErrorCodes.kt
@@ -3,22 +3,22 @@ package ca.snmc.scanner.utils
 import ca.snmc.scanner.models.Error
 
 object AppErrorCodes {
-    val EMPTY_USERNAME = Error(10001, "Please Enter a Username.")
-    val EMPTY_PASSWORD = Error(10002, "Please Enter a Password.")
-    val NULL_LOGIN_RESPONSE = Error(10003, "Login Failed. Please Try Again.")
-    val NULL_AUTHENTICATION_RESPONSE = Error(10004, "Login Failed. Please Try Again.")
-    val NULL_ORGANIZATION_DOORS_RESPONSE = Error(10005, "No Doors Found For Your Organization.")
-    val NO_INTERNET = Error(10005, "Make Sure You Have An Active Internet Connection.")
-    val PERMISSIONS_NOT_GRANTED = Error(10006, "Cannot Proceed Until Permissions Are Granted.")
-    val PERMISSIONS_NOT_GRANTED_NEVER_ASK_AGAIN = Error(10007, "Please Grant Permissions in Settings")
-    val CAMERA_ERROR = Error(10008, "An Error Occurred in Trying to Start the Camera. Please Restart the App.")
-    val INVALID_VISITOR_ID = Error(10009, "QR Code Does Not Contain a Valid Visitor ID")
-    val MULTIPLE_CODES_SCANNED = Error(10010, "Please Scan Only One Code At a Time")
+    val EMPTY_USERNAME = Error(10001, "Please enter a username.")
+    val EMPTY_PASSWORD = Error(10002, "Please enter a password.")
+    val NULL_LOGIN_RESPONSE = Error(10003, "Login failed. Please try again.")
+    val NULL_AUTHENTICATION_RESPONSE = Error(10004, "Login failed. please try again.")
+    val NULL_ORGANIZATION_DOORS_RESPONSE = Error(10005, "No doors found for your organization.")
+    val NO_INTERNET = Error(10005, "Make sure you have an active internet connection.")
+    val PERMISSIONS_NOT_GRANTED = Error(10006, "Cannot proceed until permissions are granted.")
+    val PERMISSIONS_NOT_GRANTED_NEVER_ASK_AGAIN = Error(10007, "Please grant permissions in settings")
+    val CAMERA_ERROR = Error(10008, "An error occurred while trying to start the camera. please restart the app.")
+    val INVALID_VISITOR_ID = Error(10009, "QR code does not contain a valid visitor ID.")
+    val MULTIPLE_CODES_SCANNED = Error(10010, "Please scan only one code at a time.")
 }
 
 object ApiErrorCodes {
-    val UNAUTHORIZED = Error(401, "Unauthorized")
-    val NOT_FOUND_IN_SQL_DATABASE = Error(404, "Invalid Username or Password. Please Check Your Credentials")
-    val UNVERIFIED_VISITOR = Error(402, "Unverified Visitor.")
-    val GENERAL_ERROR = Error(500, "An Error Occurred in the Server.")
+    val UNAUTHORIZED = Error(401, "Unauthorized.")
+    val NOT_FOUND_IN_SQL_DATABASE = Error(404, "Invalid username or password. Please check your credentials.")
+    val UNVERIFIED_VISITOR = Error(402, "Unverified visitor.")
+    val GENERAL_ERROR = Error(500, "An error occurred in the server.")
 }
