f629d03067879fea0df0cc78d46ae82da28180bc
ghufrannoor2 <ghufrannoor@gmail.com>
2020-08-29 15:49:14 -0600

    Removed log statements, optimized imports, confirmed that feature is working

--- Scanner/Android/app/src/main/java/ca/snmc/scanner/models/VisitLogUploadProgress.kt
+++ Scanner/Android/app/src/main/java/ca/snmc/scanner/models/VisitLogUploadProgress.kt
@@ -4,6 +4,7 @@ data class VisitLogUploadProgress(
     var progress: Int = 0,
     var timeout: Boolean = false,
     var uploadedItems: Int = 0,
-    var totalItems: Int = 0
+    var totalItems: Int = 0,
+    var success: Boolean = false
 ) {
 }
