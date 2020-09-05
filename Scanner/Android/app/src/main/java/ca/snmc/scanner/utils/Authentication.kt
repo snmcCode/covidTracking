0f70eea6ab23a86775975b79a0fef1ad5402f0e9
ghufrannoor2 <ghufrannoor@gmail.com>
2020-09-04 18:40:19 -0600

    Restored AuthenticationUtils

--- Scanner/Android/app/src/main/java/ca/snmc/scanner/utils/Authentication.kt
+++ Scanner/Android/app/src/main/java/ca/snmc/scanner/utils/Authentication.kt
@@ -1,19 +1,12 @@
 package ca.snmc.scanner.utils
 
-// Test
-private const val BREATHING_ROOM: Long = 10 * 1000
-
-//// Breathing room of 5 minutes
-//private const val BREATHING_ROOM: Long = 5 * 60 * 1000
+// Breathing room of 5 minutes
+private const val BREATHING_ROOM: Long = 5 * 60 * 1000
 
 fun isAccessTokenExpired(accessTokenExpiryTime: Long) : Boolean {
-    // Test
     return (accessTokenExpiryTime - System.currentTimeMillis()) <= BREATHING_ROOM
-//    return (accessTokenExpiryTime - System.currentTimeMillis()) <= BREATHING_ROOM
 }
 
 fun getAccessTokenExpiryTime(expiryTime: Int) : Long {
-    // Test
-    return System.currentTimeMillis() + (30 * 1000)
-//    return System.currentTimeMillis() + (expiryTime * 1000)
+    return System.currentTimeMillis() + (expiryTime * 1000)
 }
