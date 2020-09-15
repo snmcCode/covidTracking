8c8138a9be3034130bd6b1140fb249c2df6154f1
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-09-15 19:02:52 -0400

    updates to the event management

--- database-sql/dbo/StoredProcedures/event_Update.sql
+++ database-sql/dbo/StoredProcedures/event_Update.sql
@@ -1,11 +1,12 @@
 CREATE OR ALTER PROCEDURE event_Update
 (@id INT,@eventName NVARCHAR(100),
 @eventDateTime DATETIME2(7),@hall NVARCHAR(50),
-@capacity TINYINT)
+@capacity TINYINT,@isPrivate BIT)
 AS
 UPDATE event
 SET Name = COALESCE(@eventName,Name),
 DateTime=COALESCE(@eventDateTime,DateTime),
 Hall=COALESCE(@hall,Hall),
-Capacity=COALESCE(@capacity,Capacity)
+Capacity=COALESCE(@capacity,Capacity),
+IsPrivate=COALESCE(@isPrivate,ISPrivate)
 WHERE Id=@id
