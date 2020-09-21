97792436b6e4c2f3819a634a46de329bd5350a41
ahmedGhub <agawi052@uottawa.ca>
2020-09-21 00:23:52 -0400

    created the createEvent azure function and the accompanied needed code in the controller, the model, as well as the the dbmanager. the code may contain fixes in index page as well

---
+++ common/Utilities/EventController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using common.Models;
+using Common.Models;
+using Common.Utilities;
+using Microsoft.Extensions.Configuration;
+
+
+namespace common.Utilities
+{
+    public class EventController
+    {
+        private IConfigurationRoot _config;
+        private Helper _helper;
+
+        public EventController(IConfigurationRoot config, Helper helper)
+        {
+            _config = config;
+            _helper = helper;
+
+        }
+
+        public  Event CreateEvent(Event myEvent)
+        {
+            DbManager dbManager = new DbManager(DbManager.DatabaseType.SQL, _config.GetConnectionString("SQLConnectionString"), _helper);
+            Event returnevent = dbManager.addEvent(myEvent);
+            return returnevent;
+           
+        }
+
+
+    }
+}
