d4453d49ceee9e544db436278eafcd4ab9012a96
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-09-24 22:59:55 -0400

    add current booking count

--- database-sql/dbo/StoredProcedures/event_register_user.sql
+++ database-sql/dbo/StoredProcedures/event_register_user.sql
@@ -1,5 +1,15 @@
 CREATE OR ALTER PROCEDURE event_register_user
 (@eventId int,@visitorId uniqueidentifier)
 AS
-INSERT INTO dbo.visitor_event(EventId,VisitorId)
-VALUES(@eventId,@visitorId)
+
+DECLARE @bookingCount INT
+SELECT @bookingCount= dbo.GetEventRegistrationCount(@eventId)
+
+DECLARE @capacity INT
+SELECT @capacity=capacity from event where Id=@eventId
+
+IF @bookingCount < @capacity
+    INSERT INTO dbo.visitor_event(EventId,VisitorId)
+    VALUES(@eventId,@visitorId)
+ELSE
+    THROW 51982, 'EVENT_FULL',1
