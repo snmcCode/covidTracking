8de88b5b5aac5b9d82cacb7824690b12b4b96486
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-10-06 20:09:00 -0400

    event grouping-sql

---
+++ database-sql/dbo/StoredProcedures/event_group.sql
@@ -0,0 +1,14 @@
+CREATE TYPE EventsTableType AS TABLE(eventId INT)  
+
+GO
+CREATE OR ALTER PROCEDURE event_group(@events EventsTableType READONLY)
+AS
+Declare @count as int
+Declare @groupID as UNIQUEIDENTIFIER
+
+Select @count=count(*) from @events
+IF @count>0
+BEGIN
+    SELECT @groupID=GroupId from dbo.event where Id=(select top 1 eventId from @events)
+    UPDATE dbo.event SET GroupId=@groupID  FROM dbo.event e join @events e1 on e.Id=e1.eventId
+END
