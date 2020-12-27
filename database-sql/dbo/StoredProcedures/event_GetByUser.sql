f23e8854c9aea4907a7838636a53382ced03f3a7
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-12-27 13:11:41 -0500

    get events for today onward

--- database-sql/dbo/StoredProcedures/event_GetByUser.sql
+++ database-sql/dbo/StoredProcedures/event_GetByUser.sql
@@ -1,7 +1,11 @@
 CREATE OR ALTER PROCEDURE event_GetByUser
 (@visitorId uniqueidentifier)
 AS
+
+Declare @startDate DATETIME2
+SET @startDate=(select CAST(SYSDATETIMEOFFSET() AT TIME ZONE 'Eastern Standard Time' AS date))
+
 SELECT e.Id as 'EventId' ,o.Name as 'Organization',e.Name as 'Event',CAST(e.DATETIME as DATE) as 'EventDate',dbo.GetEventRegistrationCount(e.Id) as 'BookingCount' 
 FROM dbo.visitor_event ve JOIN dbo.event e on ve.eventID=e.Id 
 JOIN dbo.organization o ON o.Id=e.OrgId
-WHERE ve.VisitorId=@visitorId
+WHERE ve.VisitorId=@visitorId AND e.[DateTime] >= @startDate
