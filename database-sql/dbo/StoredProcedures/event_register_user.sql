5a6651f90e144f1a96aab2e77cdd558cc48ada9d
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-12-29 00:26:47 -0500

    app lock2

--- database-sql/dbo/StoredProcedures/event_register_user.sql
+++ database-sql/dbo/StoredProcedures/event_register_user.sql
@@ -2,34 +2,44 @@ CREATE OR ALTER PROCEDURE event_register_user
 (@eventId int,@visitorId uniqueidentifier)
 AS
 BEGIN TRANSACTION
-DECLARE @lockResult INT
-Exec @lockResult=sp_getapplock @Resource='EventBooking', @LockMode = 'Exclusive',@LockTimeout=10000    
-PRINT @lockResult
+BEGIN TRY
+    DECLARE @lockResult INT
+    Exec @lockResult=sp_getapplock @Resource='EventBooking', @LockMode = 'Exclusive',@LockTimeout=10000    
+    PRINT @lockResult
 
-DECLARE @bookingCount INT
-SELECT @bookingCount= dbo.GetEventRegistrationCount(@eventId)
+    DECLARE @bookingCount INT
+    SELECT @bookingCount= dbo.GetEventRegistrationCount(@eventId)
 
-DECLARE @capacity INT,@groupId UNIQUEIDENTIFIER
-SELECT @capacity=capacity from [event] where Id=@eventId
-SELECT @groupId=groupId from [event] where Id=@eventId
+    DECLARE @capacity INT,@groupId UNIQUEIDENTIFIER
+    SELECT @capacity=capacity from [event] where Id=@eventId
+    SELECT @groupId=groupId from [event] where Id=@eventId
 
-IF @bookingCount < @capacity
-    BEGIN TRY
-            INSERT INTO dbo.visitor_event(EventId,VisitorId,EventGroupId)
-            VALUES(@eventId,@visitorId,@groupId)
-    END TRY
-    BEGIN CATCH
-        IF ERROR_NUMBER() = 2601
-        BEGIN 
-            DECLARE @count as INT 
-            Select @count=COUNT(*) FROM dbo.visitor_event where EventId=@eventId AND VisitorId=@visitorId
-            IF @count = 0
-                THROW 51983, 'BOOKED_SAME_GROUP',1
+    IF @bookingCount < @capacity
+        BEGIN TRY
+                INSERT INTO dbo.visitor_event(EventId,VisitorId,EventGroupId)
+                VALUES(@eventId,@visitorId,@groupId)
+        END TRY
+        BEGIN CATCH
+            IF ERROR_NUMBER() = 2601
+            BEGIN 
+                DECLARE @count as INT 
+                Select @count=COUNT(*) FROM dbo.visitor_event where EventId=@eventId AND VisitorId=@visitorId
+                IF @count = 0
+                    THROW 51983, 'BOOKED_SAME_GROUP',1
 
-        END
-    END CATCH
-ELSE
-    THROW 51982, 'EVENT_FULL',1
+            END
+        END CATCH
+    ELSE
+        THROW 51982, 'EVENT_FULL',1
 
-Exec sp_releaseapplock @Resource='EventBooking'
-COMMIT TRANSACTION
+    Exec sp_releaseapplock @Resource='EventBooking'
+    
+END TRY
+BEGIN CATCH
+    Exec sp_releaseapplock @Resource='EventBooking'
+    IF @@TRANCOUNT > 0  
+        ROLLBACK TRANSACTION;
+    THROW;
+END CATCH
+IF @@TRANCOUNT > 0
+    COMMIT TRANSACTION
