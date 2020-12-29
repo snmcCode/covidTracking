70cae599e392f8cd7f34b0fc002097fe4a21bfef
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2020-12-28 23:03:24 -0500

    app lock

--- database-sql/dbo/StoredProcedures/event_register_user.sql
+++ database-sql/dbo/StoredProcedures/event_register_user.sql
@@ -1,6 +1,10 @@
 CREATE OR ALTER PROCEDURE event_register_user
 (@eventId int,@visitorId uniqueidentifier)
 AS
+BEGIN TRANSACTION
+DECLARE @lockResult INT
+Exec @lockResult=sp_getapplock @Resource='EventBooking', @LockMode = 'Exclusive',@LockTimeout=10000    
+PRINT @lockResult
 
 DECLARE @bookingCount INT
 SELECT @bookingCount= dbo.GetEventRegistrationCount(@eventId)
@@ -10,19 +14,22 @@ SELECT @capacity=capacity from [event] where Id=@eventId
 SELECT @groupId=groupId from [event] where Id=@eventId
 
 IF @bookingCount < @capacity
-    BEGIN TRY 
-        INSERT INTO dbo.visitor_event(EventId,VisitorId,EventGroupId)
-        VALUES(@eventId,@visitorId,@groupId)
+    BEGIN TRY
+            INSERT INTO dbo.visitor_event(EventId,VisitorId,EventGroupId)
+            VALUES(@eventId,@visitorId,@groupId)
     END TRY
     BEGIN CATCH
         IF ERROR_NUMBER() = 2601
         BEGIN 
             DECLARE @count as INT 
             Select @count=COUNT(*) FROM dbo.visitor_event where EventId=@eventId AND VisitorId=@visitorId
-            IF @count < 0
+            IF @count = 0
                 THROW 51983, 'BOOKED_SAME_GROUP',1
 
         END
     END CATCH
 ELSE
     THROW 51982, 'EVENT_FULL',1
+
+Exec sp_releaseapplock @Resource='EventBooking'
+COMMIT TRANSACTION
