e2880fad7f855b1a846085802f08cf6ff20bd812
Mariam05 <tomariam@gmail.com>
2021-01-18 22:50:45 +0000

    [Frontend] Add audience to back of card

--- MasjidTracker/Models/EventModel.cs
+++ MasjidTracker/Models/EventModel.cs
@@ -5,27 +5,38 @@ using System.Threading.Tasks;
 
 namespace MasjidTracker.FrontEnd.Models
 {
+    [Flags]
+    public enum VisitorsAttributes
+    {
+        none = 0,
+        senior = 1,
+        member = 2,
+        board = 4,
+        youth = 8
+    };
     public class EventModel
     {
         public int Id { get; set; }
 
-        public string organization {get; set;}
+        public string organization { get; set; }
 
         public int OrgId { get; set; }
 
         public string Name { get; set; }
 
-        public DateTime DateTime {get; set; }
+        public DateTime DateTime { get; set; }
 
-        public int Capacity {get; set;}
+        public int Capacity { get; set; }
 
-        public Boolean IsPrivate {get; set;}
+        public Boolean IsPrivate { get; set; }
 
-        public string Hall {get; set;}
+        public string Hall { get; set; }
 
-        public int bookingCount {get; set;}
+        public int bookingCount { get; set; }
 
-        public string groupId {get; set;}
+        public string groupId { get; set; }
+
+        public int targetAudience { get; set; }
 
     }
 }
