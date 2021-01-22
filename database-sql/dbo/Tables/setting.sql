e6c3a0f542970b00e290a931f486bf5a94c3e753
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2021-01-22 15:59:32 -0500

    sql schema changes

--- database-sql/dbo/Tables/setting.sql
+++ database-sql/dbo/Tables/setting.sql
@@ -3,5 +3,5 @@
     [Domain] VARCHAR(100) NOT NULL, 
     [Key] VARCHAR(50) NOT NULL, 
     [Value] NVARCHAR(2000) NOT NULL, 
-    CONSTRAINT [PK_settings] PRIMARY KEY ([Domain], [Key]) 
+    CONSTRAINT [PK_setting] PRIMARY KEY ([Domain], [Key]) 
 )
