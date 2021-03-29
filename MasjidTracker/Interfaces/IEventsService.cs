b6ecd09ff4eb12856192310921db61d3ec980481
Mariam05 <tomariam@gmail.com>
2021-03-29 19:39:16 +0000

    [Frontend] Cache statuses

--- MasjidTracker/Interfaces/IEventsService.cs
+++ MasjidTracker/Interfaces/IEventsService.cs
@@ -8,7 +8,6 @@ namespace FrontEnd.Interfaces
     public interface IEventsService
     {
         Task<List<EventModel>> GetEvents(string url, string targetResource);
-        Task<List<StatusModel>> GetStatuses(string url, string targetResource);
         Task<int> RegisterInEvent(string url, string targetResource, string jsonBody);
         Task<string> UnregisterFromEvent(string url, string targetResource, string jsonBody);
         Task<string> UpdateEvent(string url, string targetResource, string jsonBody);
