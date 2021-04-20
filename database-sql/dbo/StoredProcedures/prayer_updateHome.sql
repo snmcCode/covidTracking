b9b25086da2b2d9fe0c3c3a9ff6850771a520020
Mohamed Sharaf <Mohamed.Sharaf@live.ca>
2021-04-20 16:22:49 -0400

    add fajr azan

--- database-sql/dbo/StoredProcedures/prayer_updateHome.sql
+++ database-sql/dbo/StoredProcedures/prayer_updateHome.sql
@@ -1,16 +1,17 @@
 CREATE OR ALTER PROCEDURE prayer_updateHome @domainName varchar(100)='checkin.mysnmc.ca',
-@fajr CHAR(5),@shorooq CHAR(5),@dhuhr CHAR(5),@asr CHAR(5),@maghreb CHAR(5),@isha CHAR(5)
+@fajr CHAR(5),@fajrIqama CHAR(5),@shorooq CHAR(5),@dhuhrIqama CHAR(5),@asrIqama CHAR(5),@maghrebIqama CHAR(5),@ishaIqama CHAR(5)
 AS
 
 DECLARE @valueString VARCHAR(2000)='<div style="text-align: left;font-weight: bold">
 Prayer times <br/>
 <ul style="text-align: left">
-<li>Fajr   : '+ @fajr +' </li>
+<li>Fajr  : '+ @fajr +' </li>
+<li>Fajr Iqama  : '+ @fajrIqama +' </li>
 <li>Sunrise   : '+ @shorooq +' </li>
-<li>Dhuhr  : '+ @dhuhr +' </li>
-<li>Asr    : '+ @asr +'</li>
-<li>Maghreb: '+ @maghreb +' </li>
-<li>Isha   : '+ @isha +' </li>
+<li>Dhuhr  : '+ @dhuhrIqama +' </li>
+<li>Asr    : '+ @asrIqama +'</li>
+<li>Maghreb: '+ @maghrebIqama +' </li>
+<li>Isha   : '+ @ishaIqama +' </li>
 </ul></div>'
 
 update setting set Value=@valueString
