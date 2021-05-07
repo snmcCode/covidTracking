8d0af46e1fa098ade2d613ec376e19e810b243be
Ahmed Ladhar <aladhar@cmhc-schl.gc.ca>
2021-05-07 19:06:48 -0400

    initial commit

---
+++ BackEnd/LogVisitWorker.cs
@@ -0,0 +1,59 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Common.Models;
+using Newtonsoft.Json;
+using Common.Utilities;
+using Microsoft.Extensions.Configuration;
+using Common.Utilities.Exceptions;
+using Common.Resources;
+
+namespace BackEnd
+{
+    public  class LogVisitWorker
+    {
+        private readonly IConfiguration config;
+
+        public LogVisitWorker(IConfiguration config)
+        {
+            this.config = config;
+        }
+        [FunctionName("LogVisitWorker")]
+        public async void Run([QueueTrigger("%VisitsQueue%", Connection = "StorageConnectionString")]string myQueueItem, ILogger log)
+        {
+            LoggerHelper helper = new LoggerHelper(log, "LogVisitWorker", "QueueTrigger", "visits");
+            // helper.DebugLogger.LogCustomInformation("received message " + myQueueItem);
+            helper.DebugLogger.LogInvocation();
+            try
+            {
+                Visit visit = JsonConvert.DeserializeObject<Visit>(myQueueItem);
+                DatabaseManager databaseManager = new DatabaseManager( helper, config );
+                databaseManager.SetDataParameter(visit);
+                databaseManager.SetDataParameter(visit.Visitor);
+                await databaseManager.LogVisit();
+            }
+            catch (NoSqlDatabaseException e)
+            {
+                helper.DebugLogger.OuterException = e;
+                helper.DebugLogger.OuterExceptionType = "NoSqlDatabaseException";
+                helper.DebugLogger.Success = false;
+                helper.DebugLogger.StatusCode = CustomStatusCodes.NOSQLDATABASEERROR;
+                helper.DebugLogger.StatusCodeDescription = CustomStatusCodes.GetStatusCodeDescription(helper.DebugLogger.StatusCode);
+                helper.DebugLogger.LogFailure();
+            }
+            catch (Exception e)
+            {
+                helper.DebugLogger.OuterException = e;
+                helper.DebugLogger.OuterExceptionType = "Exception";
+                helper.DebugLogger.Description = "Generic Exception";
+                helper.DebugLogger.Success = false;
+                helper.DebugLogger.StatusCode = CustomStatusCodes.GENERALERROR;
+                helper.DebugLogger.StatusCodeDescription = CustomStatusCodes.GetStatusCodeDescription(helper.DebugLogger.StatusCode);
+                helper.DebugLogger.LogFailure();
+                log.LogError(e.Message);
+            }
+
+        }
+    }
+}
---
+++ common/Utilities/QueueControler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Common.Models;
+using Common.Utilities;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace common.Utilities
+{
+    public class QueueControler
+    {
+        private LoggerHelper Helper;
+        private readonly IConfiguration Config;
+
+
+
+        public QueueControler(LoggerHelper helper, IConfiguration config)
+        {
+            Helper = helper;
+            Config = config;
+        }
+        public async Task<string> InsertMessageAsync( Visit visit)
+        {
+            var visitBytes = System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(visit));
+            var message = System.Convert.ToBase64String(visitBytes);
+            QueueClient queue = new QueueClient(Config.GetConnectionString("StorageConnectionString"), Config.GetValue<string>("VisitsQueue"));
+            if (null != await queue.CreateIfNotExistsAsync())
+            {
+                Helper.DebugLogger.LogCustomInformation("visits-queue was created.");
+              
+            }
+
+            var  result = await queue.SendMessageAsync(message);
+            return result.Value.MessageId; 
+        }
+    }
+}
